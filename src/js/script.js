bbb3de41a53e21bb9c0241a8d61a926f85771359
vdmkhrp <vdmkhrp@gmail.com>
2024-08-24 19:45:08 +0300

    created a dev branch with the entire structure

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+const cursor = document.querySelector(".custom-cursor");
+const links = document.querySelectorAll("[data-action]");
+let isCursorInited = false;
+
+const initCursor = () => {
+  cursor.classList.add("custom-cursor--init");
+  isCursorInited = true;
+};
+
+const destroyCursor = () => {
+  cursor.classList.remove("custom-cursor--init");
+  isCursorInited = false;
+};
+
+links.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    cursor.classList.add("custom-cursor--link");
+  });
+
+  link.addEventListener("mouseout", () => {
+    cursor.classList.remove("custom-cursor--link");
+  });
+});
+
+document.addEventListener("mousemove", (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  if (!isCursorInited) {
+    initCursor();
+  }
+
+  cursor.style = `translate: ${mouseX}px ${mouseY}px`;
+});
+
+document.addEventListener("mouseout", destroyCursor);
